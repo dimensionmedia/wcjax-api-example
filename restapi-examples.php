2746dbfe7bc6efe7ec5f9631f1cea0510b9999a2
David Bisset <dbisset@dimensionmedia.com>
2016-04-09 15:06:13 -0400

    Added Example

--- restapi-examples.php
+++ restapi-examples.php
@@ -6,6 +6,9 @@ Description: Demonstration of how to modify certain aspects of the WP REST API
 Author: David Bisset
 Version: 1.0
 Author URI: http://www.davidbisset.com
+
+NOTE: This requries the WordPress 4.4+ and you should be running the latest version of the REST API plugin 2.0
+
 */
         
 /* This is an example of exposing meta data to the REST API Posts */
@@ -23,11 +26,12 @@ function ssphp_post_meta_register() {
 }
 
 function ssphp_get_post_meta( $object, $field_name, $request ) {
-    // return get_post_meta( $object[ 'id' ], $field_name, true );
-    return "manager";
+    return get_post_meta( $object[ 'id' ], $field_name, true );
 }
 
-/* This is an example of exposing meta data to the REST API Posts */
+/*  This is an example of using rest_query_vars filter to be able to query by meta_key/meta_value
+ *  Example: yoururl.com/wp-json/wp/v2/posts?filter[meta_key]=job-type&filter[meta_value]=manager
+ */
 
 function my_allow_meta_query( $valid_vars ) {
     
